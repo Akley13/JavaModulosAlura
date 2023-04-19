a92367c0981a90a781330bf2f08b151647d048e5
Akley Alves <akleyalvescr@gmail.com>
2023-04-18 23:21:40 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        System.out.println("SCREEN MATCH");
+        System.out.println("Filme: Top Gun - Maverick");
+
+        int anoLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoLancamento); // concatenação
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        System.out.println("Nota do filme: " + notaDoFilme);
+        System.out.println("Plano (True/False): " + incluidoNoPlano);
     }
 }
