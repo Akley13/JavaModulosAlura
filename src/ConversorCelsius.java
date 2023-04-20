a126bf488469f061b27e8654860760ea616105b4
Akley Alves <akleyalvescr@gmail.com>
2023-04-19 22:47:43 -0300

    Update ConversorCelsius.java

--- src/ConversorCelsius.java
+++ src/ConversorCelsius.java
@@ -1,8 +1,8 @@
 public class ConversorCelsius {
     public static void main(String[] args) {
 
-        double celsius = 20.5789;
-        double temperatura = (celsius * 1.8) + 32;
-        System.out.printf("Celsius convertido em Fahrenheit: %.2f", temperatura);
+        double temperaturaCelsius = 20.5789;
+        double temperaturaFahrenheit = (temperaturaCelsius * 1.8) + 32;
+        System.out.printf("Celsius convertido em Fahrenheit: %.2f", temperaturaFahrenheit);
     }
 }
