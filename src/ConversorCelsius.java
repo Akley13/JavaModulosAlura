128fc849320fef9b48f4edc878b022f9065c7147
Akley Alves <akleyalvescr@gmail.com>
2023-04-19 22:45:07 -0300

    Create ConversorCelsius.java

---
+++ src/ConversorCelsius.java
@@ -0,0 +1,8 @@
+public class ConversorCelsius {
+    public static void main(String[] args) {
+
+        double celsius = 20.5789;
+        double temperatura = (celsius * 1.8) + 32;
+        System.out.printf("Celsius convertido em Fahrenheit: %.2f", temperatura);
+    }
+}
