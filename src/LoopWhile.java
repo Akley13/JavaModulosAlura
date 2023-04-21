558aeac9fb3cc77cf66a0c2dc16f4b81aec68842
Akley Alves <akleyalvescr@gmail.com>
2023-04-21 17:16:32 -0300

    Update LoopWhile.java

--- src/LoopWhile.java
+++ src/LoopWhile.java
@@ -4,15 +4,19 @@ public class LoopWhile {
     public static void main(String[] args) {
         Scanner inputDados = new Scanner(System.in);
 
-        double notaDofilme = 0;
+        double notaDofilme = 1;
         double mediaAvaliacao = 0;
-        int contador = 1;
+        int contador = 0;
 
         while (notaDofilme != -1) {
-            System.out.println("Contador: " + contador++);
 
             System.out.println("Nota dos filmes: ");
             notaDofilme = inputDados.nextDouble();
+
+            if (notaDofilme != -1) {
+                mediaAvaliacao += notaDofilme;
+                contador++;
+            }
         }
 
         System.out.printf("Média da avaliação: %.2f",mediaAvaliacao / contador);
