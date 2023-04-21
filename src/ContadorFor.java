b529bcd792f49c117e67277b95f478a192b2ca5d
Akley Alves <akleyalvescr@gmail.com>
2023-04-21 17:28:08 -0300

    update

---
+++ src/ContadorFor.java
@@ -0,0 +1,7 @@
+public class ContadorFor {
+    public static void main(String[] args) {
+        for (int contador = 1; contador <= 10;) {
+            System.out.println(contador++);
+        }
+    }
+}
