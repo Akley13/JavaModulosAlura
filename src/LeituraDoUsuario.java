39364ee78083af7f3e0f12a89654b9ee3e5e77fa
Akley Alves <akleyalvescr@gmail.com>
2023-04-21 14:54:23 -0300

    Update LeituraDoUsuario.java

--- src/LeituraDoUsuario.java
+++ src/LeituraDoUsuario.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class LeituraDoUsuario {
     public static void main(String[] args) {
-        Scanner coletarDadosUsuarios = new Scanner()
+        Scanner inputData = new Scanner(System.in);
+
+        System.out.println("Me conte, qual é seu filme favorito? ");
+        String filme = inputData.nextLine();
+        System.out.println("Qual o ano de lançamento? ");
+        int anoLancamento = inputData.nextInt();
+
+        System.out.println("Filme favorito: " + filme + " | " + "Ano de lançamento: " + anoLancamento);
     }
 }
