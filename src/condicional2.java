78f8524f17d2294938b3cf6c3e47b37dd6f68706
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 22:18:54 -0300

    Update condicional2.java

--- src/condicional2.java
+++ src/condicional2.java
@@ -1,15 +1,22 @@
 public class condicional2 {
     public static void main(String[] args) {
         int anoDeLancamento = 2002;
-        boolean incluidoNoPlano = true;
+        boolean incluidoNoPlano = false;
         double nota = 9.5;
+        String tipoPlano = "normal";
 
-        if (anoDeLancamento > 2000 & anoDeLancamento < 2010) {
+        if (anoDeLancamento >= 2000 & anoDeLancamento <= 2010) {
             System.out.println("Filme não muito recente!");
         } else if (anoDeLancamento > 2010) {
             System.out.println("Filme recente!");
         } else {
             System.out.println("Filme antigo!");
+        }
+
+        if (incluidoNoPlano == true || tipoPlano.equals("Plus")){
+            System.out.println("Acesso permitido!");
+        } else {
+            System.out.println("Acesso negado!");
         }
     }
 }
