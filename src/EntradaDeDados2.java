1b43395ce073f24ed5115c848bc8f003d437577e
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 23:04:16 -0300

    Update EntradaDeDados2.java

--- src/EntradaDeDados2.java
+++ src/EntradaDeDados2.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.Scanner;
 
 public class EntradaDeDados2 {
@@ -7,6 +8,11 @@ public class EntradaDeDados2 {
         System.out.println("Qual seu jogo favorito?");
         String jogo = lerDados.nextLine();
 
-        System.out.println("Jogo favorito: " + jogo);
+        System.out.println("E o ano de seu lançamento?");
+        int anoDeLancamento = lerDados.nextInt();
+
+        System.out.printf("Jogo favorito: %s", jogo);
+        System.out.println();
+        System.out.printf("Ano de lançamento: %d", anoDeLancamento);
     }
 }
