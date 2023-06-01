9e64f5dbe2db35c939307da7c22f858b531f7642
Akley Alves <akleyalvescr@gmail.com>
2023-05-31 22:34:04 -0300

    Update SwitchCase.java

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -3,23 +3,42 @@ import java.util.Scanner;
 
 public class SwitchCase {
     public static void main(String[] args) {
-        int sorvete = 1;
+        Scanner entradaDeDados = new Scanner(System.in);
+
+        System.out.println("Digite o número relacionado ao sabor de sua escolha.");
+        System.out.println();
+
+        System.out.println("SABORES");
+        System.out.println();
+
+        System.out.println("1 - Morango");
+        System.out.println("2 - Limão");
+        System.out.println("3 - Chocolate");
+        System.out.println("4 - Maracujá");
+        System.out.println("0 - Cancelar operação.");
+
+
+        int sorvete = entradaDeDados.nextInt();
 
         switch (sorvete) {
+            case 0:
+                System.out.println("Operação cancelada.");
+                break;
+
             case 1:
-                System.out.println("Morango");
+                System.out.println("Sorvete sabor Morango");
                 break;
 
             case 2:
-                System.out.println("Limão");
+                System.out.println("Sorvete sabor Limão");
                 break;
 
             case 3:
-                System.out.println("Chocolate");
+                System.out.println("Sorvete sabor Chocolate");
                 break;
 
             case 4:
-                System.out.println("Maracujá");
+                System.out.println("Sorvete sabor Maracujá");
                 break;
 
             default:
