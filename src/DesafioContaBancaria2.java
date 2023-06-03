1998ab08c0de85c0806ff91973070ba077c1a235
Akley Alves <akleyalvescr@gmail.com>
2023-06-03 12:24:23 -0300

    Update DesafioContaBancaria2.java

--- src/DesafioContaBancaria2.java
+++ src/DesafioContaBancaria2.java
@@ -10,14 +10,11 @@ public class DesafioContaBancaria2 {
         double deposito = 0;
         double valorTransferido = 0;
 
-        System.out.println();
-        System.out.println("XXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
-        System.out.println();
+        System.out.println("\nXXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
 
-        System.out.println("-------------- CADASTRO DE CLIENTE ----------------");
-        System.out.println();
+        System.out.println("\n-------------- CADASTRO DE CLIENTE ----------------");
 
-        System.out.println("Nome Completo: ");
+        System.out.println("\nNome Completo: ");
         String nome = inserirDados.nextLine();
 
         System.out.println("CPF: ");
@@ -26,81 +23,66 @@ public class DesafioContaBancaria2 {
         System.out.println("Saldo inicial: ");
         double saldo = inserirDados.nextInt();
 
-        System.out.println();
+        System.out.println("\n-------------- CLIENTE CADASTRADO -----------------");
 
-        System.out.println("-------------- CLIENTE CADASTRADO -----------------");
-        System.out.println();
-
-        System.out.println("Nome completo: " + nome);
+        System.out.println("\nNome completo: " + nome);
         System.out.println("CPF: " + cpf);
         System.out.println("Saldo inicial R$ " + saldo);
 
-        System.out.println();
-        System.out.println("XXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
-        System.out.println();
+        System.out.println("\nXXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
 
-        System.out.println("------------------- OPERAÇÕES ---------------------");
-        System.out.println();
+        System.out.println("\n------------------- OPERAÇÕES ---------------------");
 
-        System.out.println("SELECIONE A OPÇÃO DESEJADA:");
-        System.out.println();
+        System.out.println("\nSELECIONE A OPÇÃO DESEJADA:");
 
         while (operacao != 4) {
 
-            System.out.println("1- Consultar saldo");
+            System.out.println("\n1- Consultar saldo");
             System.out.println("2- Receber depósito");
             System.out.println("3- Transferir valor");
             System.out.println("4- Encerrar operação");
             System.out.println();
 
             operacao = inserirDados.nextInt();
-            System.out.println();
-            System.out.println("OPÇÃO: " + operacao);
+            System.out.println("\nOPÇÃO: " + operacao);
 
             if (operacao > 4){
-                System.out.println("Opção Inválida!");
-                System.out.println();
+                System.out.println("\nOpção Inválida!");
             }
 
             switch (operacao){
                 case 1:
-                    System.out.println("Saldo atual R$ " + saldo);
-                    System.out.println();
+                    System.out.println("\nSaldo atual R$ " + saldo);
                     break;
 
                 case 2:
-                    System.out.println("Valor do depósito: ");
+                    System.out.println("\nValor do depósito: ");
                     deposito = inserirDados.nextDouble();
 
                     saldo += deposito;
 
-                    System.out.println("Depósito realizado | Saldo atualizado R$ " + saldo);
-                    System.out.println();
+                    System.out.println("\nDepósito realizado | Saldo atual R$ " + saldo);
                     break;
 
                 case 3:
-                    System.out.println("Valor a ser transferido: ");
+                    System.out.println("\nValor a ser transferido: ");
                     valorTransferido = inserirDados.nextDouble();
-                    System.out.println();
 
                     if (valorTransferido > saldo){
-                        System.out.println("Saldo insuticiente | Saldo atual: R$ " + saldo);
-                        System.out.println();
+                        System.out.println("\nSaldo insuticiente | Saldo atual: R$ " + saldo);
                         break;
                     }
 
                     saldo -= valorTransferido;
 
-                    System.out.println("Valor da transferência | R$ " + valorTransferido);
+                    System.out.println("\nValor da transferência | R$ " + valorTransferido);
                     System.out.println("Saldo Atualizado | R$ " + saldo);
-
-                    System.out.println();
                     break;
 
                 case 4:
-                    System.out.println("OPERAÇÃO ENCERRADA.");
-                    System.out.println();
-                    System.out.println("XXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
+                    System.out.println("\nOPERAÇÃO ENCERRADA.");
+
+                    System.out.println("\nXXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
                     break;
             }
         }
