60fb3cefac46b323f605686e2c8e444696e8321f
Akley Alves <akleyalvescr@gmail.com>
2023-06-02 21:04:06 -0300

    Update DesafioContaBancaria2.java

--- src/DesafioContaBancaria2.java
+++ src/DesafioContaBancaria2.java
@@ -1,13 +1,11 @@
+import javax.sound.midi.Soundbank;
 import java.util.Scanner;
 
 public class DesafioContaBancaria2 {
     public static void main(String[] args) {
         Scanner inserirDados = new Scanner(System.in);
 
-        String nome;
-        int cpf;
-        double saldo;
-
+        System.out.println();
         System.out.println("XXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
         System.out.println();
 
@@ -15,10 +13,25 @@ public class DesafioContaBancaria2 {
         System.out.println();
 
         System.out.println("Nome Completo: ");
+        String nome = inserirDados.nextLine();
+
         System.out.println("CPF: ");
+        String cpf = inserirDados.nextLine();
+
         System.out.println("Saldo inicial: ");
+        double saldo = inserirDados.nextInt();
+
+        System.out.println("-------------- CLIENTE CADASTRADO ----------------");
+        System.out.println();
+
+        System.out.println("Nome completo: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Saldo inicial: R$ " + saldo);
 
         System.out.println();
         System.out.println("XXXXXXXXXXXXXXXXXXXX KESS BANK XXXXXXXXXXXXXXXXXXXX");
+
+        
+
     }
 }
