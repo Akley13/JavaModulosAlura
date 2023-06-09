5e83fe60cb879c5529fef8e7a75e0afdb1c5ed60
Akley Alves <akleyalvescr@gmail.com>
2023-06-08 21:52:31 -0300

    att

--- src/PessoaGetESet.java
+++
@@ -1,7 +0,0 @@
-public class PessoaGetESet {
-    private String nome;
-    private int idade;
-    private double altura;
-
-    void  
-}
--- src/PessoaGetESet_principal.java
+++
@@ -1,8 +0,0 @@
-public class PessoaGetESet_principal {
-    public static void main(String[] args) {
-        PessoaGetESet pessoa = new PessoaGetESet();
-
-        pessoa.nome = "Akley";
-        System.out.println(pessoa.nome);
-    }
-}
