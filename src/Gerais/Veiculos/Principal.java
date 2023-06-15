c2f03cc152ff49154753eb0916989c590456c389
Akley Alves <akleyalvescr@gmail.com>
2023-06-14 21:40:51 -0300

    Update Principal.java

--- src/Gerais/Veiculos/Principal.java
+++ src/Gerais/Veiculos/Principal.java
@@ -3,6 +3,7 @@ package Gerais.Veiculos;
 public class Principal {
     public static void main(String[] args) {
 
+        // CARROS
         VeiculosGerais bmwI4 = new VeiculosGerais();
         bmwI4.setNome("BMW i4");
         bmwI4.setMarca("BMW");
@@ -13,5 +14,17 @@ public class Principal {
         bmwI4.setQuantidadePassageiros(5);
 
         bmwI4.exibeFichaVeiculosGerais();
+
+
+        // MOTOS
+        Motos breakout117 = new Motos();
+        breakout117.setNome("Breakout 117");
+        breakout117.setMarca("Harley Davidson");
+        breakout117.setCategoria("");
+        breakout117.setAnoDeCriacao(2022);
+        breakout117.setVelocidadeMaxima("225 Km/h");
+        breakout117.setCombustivel("Elétrico");
+        breakout117.setQuantidadePassageiros(5);
+
     }
 }
