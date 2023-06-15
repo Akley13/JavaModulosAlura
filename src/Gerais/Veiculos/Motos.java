9c9c0b063d7f1d6c8030be0738b3602e0f4c58be
Akley Alves <akleyalvescr@gmail.com>
2023-06-14 21:30:23 -0300

    att

--- src/Gerais/Veiculos/Motos.java
+++ src/Gerais/Veiculos/Motos.java
@@ -1,7 +1,28 @@
 package Gerais.Veiculos;
 
-import Gerais.Veiculos.VeiculosGerais;
-
 public class Motos extends VeiculosGerais {
     private int quantidadeDeRodas;
+    private int cilindradas;
+
+    public int getQuantidadeDeRodas() {
+        return quantidadeDeRodas;
+    }
+
+    public void setQuantidadeDeRodas(int quantidadeDeRodas) {
+        this.quantidadeDeRodas = quantidadeDeRodas;
+    }
+
+    public int getCilindradas() {
+        return cilindradas;
+    }
+
+    public void setCilindradas(int cilindradas) {
+        this.cilindradas = cilindradas;
+    }
+
+    public void exibeDadosMotos(){
+        System.out.println("------ INFORMAÇÕES ADICIONAIS ------");
+        System.out.println("\nQuantidade de rodas: ");
+        System.out.println("Cilindradas: ");
+    }
 }
